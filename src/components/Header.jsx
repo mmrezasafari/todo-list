00b974e8bec8eef5442f2344d43af13f6c6dac33
mmrezasafari <mohammadrezasafari32@gmail.com>
2022-04-19 14:55:58 +0430

    create Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import lightIcon from "../assets/images/icon-sun.svg";
 import darkIcon from "../assets/images/icon-moon.svg";
 
+
 function Header() {
   return (
     <header className="header">
       <div className="header-div">
         <h1 className="header-text">T O D O</h1>
-        <img src={lightIcon} className="theme-icon"/>
       </div>
     </header>
   );
