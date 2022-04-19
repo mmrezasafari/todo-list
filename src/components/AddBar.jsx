e1d50e32d7bc70f0e5b80f426f0f9069d9b97dc5
mmrezasafari <mohammadrezasafari32@gmail.com>
2022-04-19 14:55:42 +0430

    create AddBar

--- src/components/AddBar.jsx
+++ src/components/AddBar.jsx
@@ -1,12 +1,22 @@
-function AddBar() {
+
+function AddBar({ state, onChange, onClick, keyDown }) {
   return (
     <section className="add-bar-section">
       <div className="add-bar-div">
-        <input type="text" className="add-input" placeholder="create new todo..." />
-        <button type="button" className="add-button">ADD!</button>
+        <input
+          type="text"
+          className="add-input"
+          placeholder="create new todo..."
+          value={state}
+          onChange={onChange}
+          onKeyDown={keyDown}
+        />
+        <button type="button" className="add-button" onClick={onClick}>
+          ADD !
+        </button>
       </div>
     </section>
   );
 }
 
-export default AddBar ;
+export default AddBar;
